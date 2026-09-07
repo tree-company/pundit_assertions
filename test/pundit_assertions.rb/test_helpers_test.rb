93dd7efd57bad83377e68513363defeee044ff70
Robbe Van Petegem <git@robbevp.be>
2026-09-07 09:22:47 +0200

    Add custom error messages to assertions (#2)
    * Add custom error messages to our assertions
    
    This improves the feedback that you get during tests. A failing assertion will now provide clear feedback on what failed.
    As a part of this, I added a few extra aliases for some methods and added more extensive tests.
    
    * Fix methods in readme
    
    * Clarify comment
    
    * Remove aliases
    
    * Improve logging of user object

--- test/pundit_assertions.rb/test_helpers_test.rb
+++ test/pundit_assertions.rb/test_helpers_test.rb
@@ -18,6 +18,10 @@ module PunditAssertions
     def self.collection
       @collection ||= []
     end
+
+    def self.reset!
+      @collection = []
+    end
   end
 
   class MockPolicy
@@ -57,7 +61,7 @@ module PunditAssertions
   end
 end
 
-class PunditAssertions::MockPolicyTest < Minitest::Test
+class PunditAssertions::MockPolicyTest < Minitest::Test # rubocop:disable Metrics/ClassLength
   include PunditAssertions::TestHelpers
 
   def setup
@@ -65,36 +69,168 @@ class PunditAssertions::MockPolicyTest < Minitest::Test
     @comment = PunditAssertions::Comment.new
   end
 
-  def test_assert_permitted
-    assert_permitted @user, @comment, :index
-    assert_not_permitted nil, @comment, :index
-    refute_permitted nil, @comment, :index
+  def teardown
+    PunditAssertions::Comment.reset!
   end
 
-  def test_that_returns_bool_for_permitted
+  def test_assert_permitted
+    assert_permitted @user, @comment, :index
+    error = assert_raises Minitest::Assertion do
+      assert_permitted nil, @comment, :index
+    end
+
+    assert_equal "Expected nil to be permitted to index #{@comment}", error.message
+  end
+
+  def test_assert_not_permitted
+    assert_not_permitted nil, @comment, :index
+    error = assert_raises Minitest::Assertion do
+      assert_not_permitted @user, @comment, :index
+    end
+    assert_equal "Expected PunditAssertions::User to not be permitted to index #{@comment}", error.message
+  end
+
+  def test_refute_permitted
+    refute_permitted nil, @comment, :index
+    error = assert_raises Minitest::Assertion do
+      refute_permitted @user, @comment, :index
+    end
+    assert_equal "Expected PunditAssertions::User to not be permitted to index #{@comment}", error.message
+  end
+
+  def test_permitted
     assert_equal true, permitted?(@user, @comment, :index)
     assert_equal false, permitted?(nil, @comment, :index)
   end
 
   def test_assert_has_permitted_attributes
     assert_has_permitted_attributes @user, @comment
-    assert_no_permitted_attributes nil, @comment
+    error = assert_raises Minitest::Assertion do
+      assert_has_permitted_attributes nil, @comment
+    end
+    assert_includes error.message, 'Expected nil to have permitted attributes'
   end
 
   def test_assert_has_permitted_attributes_with_action
     assert_has_permitted_attributes @user, @comment, :create
+    error = assert_raises Minitest::Assertion do
+      assert_has_permitted_attributes nil, @comment, :create
+    end
+    assert_includes error.message, 'Expected nil to have permitted attributes for create'
+  end
+
+  def test_assert_no_permitted_attributes
+    assert_no_permitted_attributes nil, @comment
+    error = assert_raises Minitest::Assertion do
+      assert_no_permitted_attributes @user, @comment
+    end
+    assert_includes error.message, 'Expected PunditAssertions::User to not have permitted attributes'
+    assert_includes error.message, 'Attributes [:content] were permitted'
+  end
+
+  def test_assert_no_permitted_attributes_with_action
     assert_no_permitted_attributes nil, @comment, :create
+    error = assert_raises Minitest::Assertion do
+      assert_no_permitted_attributes @user, @comment, :create
+    end
+    assert_includes error.message, 'Expected PunditAssertions::User to not have permitted attributes for create'
+    assert_includes error.message, 'Attributes [:content, :hidden] were permitted'
+  end
+
+  def test_refute_permitted_attributes
+    refute_permitted_attributes nil, @comment
+    error = assert_raises Minitest::Assertion do
+      refute_permitted_attributes @user, @comment
+    end
+    assert_includes error.message, 'Expected PunditAssertions::User to not have permitted attributes'
+    assert_includes error.message, 'Attributes [:content] were permitted'
   end
 
   def test_assert_attribute_permitted
     assert_attributes_permitted @user, @comment, :content
-    assert_attributes_permitted @user, @comment, %i[content hidden], :create
-    assert_not_attributes_permitted @user, @comment, :user
+    error = assert_raises Minitest::Assertion do
+      assert_attributes_permitted nil, @comment, :content
+    end
+    assert_includes error.message, 'Expected nil to have [:content] in permitted attributes'
   end
 
-  def test_assert_scope
+  def test_assert_attribute_permitted_with_action
+    assert_attributes_permitted @user, @comment, %i[content hidden], :create
+    error = assert_raises Minitest::Assertion do
+      assert_attributes_permitted nil, @comment, :content, :create
+    end
+    assert_includes error.message, 'Expected nil to have [:content] in permitted attributes for create'
+  end
+
+  def test_assert_not_attributes_permitted
+    assert_not_attributes_permitted @user, @comment, :hidden
+    assert_not_attributes_permitted nil, @comment, :content
+    error = assert_raises Minitest::Assertion do
+      assert_not_attributes_permitted @user, @comment, %i[content user]
+    end
+    assert_includes error.message, 'Expected PunditAssertions::User to not have [:content] in permitted attributes'
+  end
+
+  def test_assert_not_attributes_permitted_with_action
+    assert_not_attributes_permitted @user, @comment, :user, :create
+    assert_not_attributes_permitted nil, @comment, :content, :create
+    error = assert_raises Minitest::Assertion do
+      assert_not_attributes_permitted @user, @comment, %i[content hidden user], :create
+    end
+    assert_includes error.message,
+                    'Expected PunditAssertions::User to not have [:content, :hidden] in permitted attributes for create'
+  end
+
+  def test_refute_attributes_permitted
+    refute_attributes_permitted @user, @comment, :hidden
+    refute_attributes_permitted nil, @comment, :content
+    error = assert_raises Minitest::Assertion do
+      refute_attributes_permitted @user, @comment, :content
+    end
+    assert_includes error.message, 'Expected PunditAssertions::User to not have [:content] in permitted attributes'
+  end
+
+  def test_permitted_attributes
+    assert_equal [:content], permitted_attributes(@user, @comment)
+    assert_equal %i[content hidden], permitted_attributes(@user, @comment, :create)
+    assert_nil permitted_attributes(nil, @comment)
+    assert_nil permitted_attributes(nil, @comment, :create)
+  end
+
+  def test_scope
+    assert_equal [@comment], scope(@user, PunditAssertions::Comment)
+    assert_empty scope(nil, PunditAssertions::Comment)
+  end
+
+  def test_assert_scope_includes
     assert_scope_includes @user, @comment
-    assert_scope_not_includes @user, PunditAssertions::Comment.new(hidden: true)
-    assert_scope_empty nil, [@comment]
+    error = assert_raises Minitest::Assertion do
+      assert_scope_includes nil, @comment
+    end
+    assert_includes error.message, "Expected #{@comment} to be included in the scope for nil"
+  end
+
+  def test_assert_not_scope_includes
+    assert_not_scope_includes @user, PunditAssertions::Comment.new(hidden: true)
+    error = assert_raises Minitest::Assertion do
+      assert_not_scope_includes @user, @comment
+    end
+    assert_includes error.message, "Expected #{@comment} to not be included in the scope for PunditAssertions::User"
+  end
+
+  def test_refute_scope_includes
+    refute_scope_includes @user, PunditAssertions::Comment.new(hidden: true)
+    error = assert_raises Minitest::Assertion do
+      refute_scope_includes @user, @comment
+    end
+    assert_includes error.message, "Expected #{@comment} to not be included in the scope for PunditAssertions::User"
+  end
+
+  def test_assert_scope_empty
+    assert_scope_empty nil, PunditAssertions::Comment
+    error = assert_raises Minitest::Assertion do
+      assert_scope_empty @user, PunditAssertions::Comment
+    end
+    assert_includes error.message, 'Expected scope for PunditAssertions::User to be empty'
   end
 end
